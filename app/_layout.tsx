01ef22a5cf6fb9bd0799b4ca8a263a159f26100e
Maluks101 <ST10496079@imconnect.edu.za>
2025-11-13 11:27:06 +0200

    added layout file and and courses filter

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,7 @@ import index from "@/app/index"
 import AddMenuScreen from "@/app/AddMenuScreen";
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import CoursesScreen from "@/app/CoursesScreen";
 import { Ionicons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
@@ -24,12 +25,14 @@ const App = () => {
             let iconName: keyof typeof Ionicons.glyphMap = "home";
             if (route.name === "Home") iconName = "home";
             else if (route.name === "Add Menu") iconName = "add-circle";
+            else if (route.name === "Courses") iconName = "restaurant";
             return <Ionicons name={iconName} size={size} color={color} />;
           },
         })}
       >
         <Tab.Screen name="Home" component={index} />
         <Tab.Screen name="Add Menu" component={AddMenuScreen} />
+        <Tab.Screen name="Courses" component={CoursesScreen} />
       </Tab.Navigator>
     </MealsProvider>
   );
